c76298c493845cee4e709b7cd6e29ee9b8757812
Ygor Andreff <iandreff@gmail.com>
2021-06-09 17:07:24 -0600

    Add project files.

---
+++ Queries/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var query = from c in context.Courses
+                                       where c.Name.Contains("c#")
+                                       orderby c.Name
+                                       select c;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+                        
+        }
+    }
+}
