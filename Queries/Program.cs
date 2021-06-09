bc2146def83e39e3b4049a28f7cdb6543b756ecb
Ygor Andreff <iandreff@gmail.com>
2021-06-09 17:13:16 -0600

    Initial commit

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,10 +10,12 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            var query = from c in context.Courses
-                                       where c.Name.Contains("c#")
-                                       orderby c.Name
-                                       select c;
+            //var query = from c in context.Courses
+            //                           where c.Name.Contains("c#")
+            //                           orderby c.Name
+            //                           select c;
+
+            var query = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
 
             foreach (var item in query)
             {
