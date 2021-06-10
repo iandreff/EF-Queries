3cd05ca006dcd86faf0f52f753bd03e24ee2b7c0
Ygor Andreff <iandreff@gmail.com>
2021-06-09 18:29:07 -0600

    Added some linq examples about select, where, join, order

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,18 +10,47 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            //var query = from c in context.Courses
-            //                           where c.Name.Contains("c#")
-            //                           orderby c.Name
-            //                           select c;
+            var query = from c in context.Courses
+                        where c.Name.Contains("c#")
+                        orderby c.Name
+                        select c;
 
-            var query = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
+            var query1 = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
 
             foreach (var item in query)
             {
                 Console.WriteLine(item.Name);
             }
-                        
+
+            var anonimus = from c in context.Courses
+                           where c.Author.Id == 1
+                           orderby c.Level descending, c.Name
+                           select new { Name = c.Name, Author = c.Author.Name };
+
+            var groupBy = from c in context.Courses
+                          group c by c.Level into g
+                          select g;
+
+            foreach (var gr in groupBy)
+            {
+                Console.WriteLine(gr.Key);
+                foreach (var i in gr)
+                {
+                    Console.WriteLine(i.Name);
+                }
+
+            }
+
+            foreach (var item in groupBy)
+            {
+                Console.WriteLine("{0}, ({1})", item.Key, item.Count());
+            }
+
+            var queryJoins = from c in context.Courses
+                             join a in context.Authors on c.AuthorId equals a.Id
+                             select new { courseName = c.Name, AuthorName = a.Name };
+
+
         }
     }
 }
