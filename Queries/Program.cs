19cf73f5efff704f3741e243e1c0b7d3fffc0ea2
Ygor Andreff <iandreff@gmail.com>
2021-06-10 10:01:52 -0600

    second commit

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Linq;
+using System.Data.Entity;
 
 namespace Queries
 {
@@ -49,6 +50,22 @@ namespace Queries
             var queryJoins = from c in context.Courses
                              join a in context.Authors on c.AuthorId equals a.Id
                              select new { courseName = c.Name, AuthorName = a.Name };
+
+            var groupJoin = from a in context.Authors
+                            join c in context.Courses on a.Id equals c.AuthorId into g
+                            select new {authorName=a.Name, courses=g.Count() };
+
+            foreach (var item in groupJoin)
+            {
+                Console.WriteLine("{0}, ({1})", item.authorName, item.courses);
+            }
+
+            var courses23 = context.Courses.Include(c=>c.Author).ToList();
+
+            foreach (var item in courses23)
+            {
+                Console.WriteLine("{0}, ({1})", item.Name, item.Author.Name);
+            }
 
 
         }
